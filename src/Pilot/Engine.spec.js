8d3b2338acfa3a9177b3e3fe53f46677b2c261ff
Alexandre Martinez <alexandre.martinez@benextcompany.com>
2018-05-04 11:37:38 +0200

    Refactor test for conciseness

--- src/Pilot/Engine.spec.js
+++ src/Pilot/Engine.spec.js
@@ -8,16 +8,28 @@ function getActions(program, world) {
     return action;
 }
 
+function expectIdleAction(program, world) {
+    const actions = getActions(program, world);
+    expect(actions.length).toBe(0);
+}
+
+function expectSingleAction(program, world, expectedAction) {
+    const actions = getActions(program, world);
+    expect(actions.length).toBe(1);
+    const [action] = actions;
+    expect(action).toEqual(expectedAction);
+}
+
 describe('Pilot Engine', () => {
     it('should provide null action if empty program', () => {
         const program = {};
         const world = {};
-        const actions = getActions(program, world);
-        expect(actions.length).toBe(0);
+        expectIdleAction(program, world);
     });
 
-    describe('forward component', () => {
+    describe('Forward component', () => {
         it('should provide IDLE action if not connected to output', () => {
+            const world = {};
             const program = {
                 output: 'x2',
                 components: [{
@@ -26,12 +38,11 @@ describe('Pilot Engine', () => {
                     power: 1
                 }]
             };
-            const world = {};
-            const actions = getActions(program, world);
-            expect(actions.length).toBe(0);
+            expectIdleAction(program, world);
         });
 
         it('should provide THRUST action if connected to output', () => {
+            const world = {};
             const program = {
                 output: 'x1',
                 components: [{
@@ -40,15 +51,15 @@ describe('Pilot Engine', () => {
                     power: 1
                 }]
             };
-            const world = {};
-            const actions = getActions(program, world);
-            expect(actions.length).toBe(1);
-            const [action] = actions;
-            expect(action.type).toBe(Action.THRUST);
-            expect(action.power).toBe(1);
+            const expectedAction = {
+                type: Action.THRUST,
+                power: 1
+            };
+            expectSingleAction(program, world, expectedAction);
         });
 
         it('should provide THRUST action if connected to output, power 2', () => {
+            const world = {};
             const program = {
                 output: 'x1',
                 components: [{
@@ -57,15 +68,27 @@ describe('Pilot Engine', () => {
                     power: 2
                 }]
             };
-            const world = {};
-            const actions = getActions(program, world);
-            expect(actions.length).toBe(1);
-            const [action] = actions;
-            expect(action.type).toBe(Action.THRUST);
-            expect(action.power).toBe(2);
+            const expectedAction = {
+                type: Action.THRUST,
+                power: 2
+            };
+            expectSingleAction(program, world, expectedAction);
         });
+    });
 
-
-    })
+    describe('Turn component', ()=>{
+        it('should return IDLE if not connected to output', () => {
+            const world = {};
+            const program = {
+                output: 'x2',
+                components: [{
+                    label: 'x1',
+                    type: Component.TURN,
+                    power: 1
+                }]
+            };
+            expectIdleAction(program, world);
+        })
+    });
 
 })
